500e20b9c6f4f9652a8c318ea8cbbddb19a6f841
JYOTHIKANAVYASRICHEGONDI <85726266+jyothikachegondi@users.noreply.github.com>
2023-07-19 15:28:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function bulboff(){
+    document.getElementById("bulb").src="https://freepngimg.com/save/31669-bulb-off-transparent-image/500x500";
+}
+function bulbon(){
+    document.getElementById("bulb").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXCPKLKm2sWUs5c_UN2qXNNm7hIDin5zv0Aw&usqp=CAU";
+}
